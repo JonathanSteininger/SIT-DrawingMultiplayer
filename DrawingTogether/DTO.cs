075bb32bbab1fe1228e006dac494ccf3e25a0325
Jonathan Steininger <jonathan.steininger@icloud.com>
2023-06-25 20:28:55 +1200

    Add project files.

---
+++ DrawingTogether/DTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawingTogether
+{
+    public abstract class DTO
+    {
+        public abstract string Type { get; }
+    }
+}
