a6fa37dcfc52f5467eabf62916030461a06e5acb
Jonathan Steininger <jonathan.steininger@icloud.com>
2023-06-27 00:58:52 +1200

    made DTO an interface. made a linedata transfer class

--- DrawingTogether/Net/DTO.cs
+++ DrawingTogether/Net/DTO.cs
@@ -6,8 +6,8 @@ using System.Threading.Tasks;
 
 namespace DrawingTogether.Net
 {
-    public abstract class DTO
+    public interface DTO
     {
-        public abstract string Type { get; }
+        string Type { get; }
     }
 }
---
+++ DrawingTogether/Net/LineDataTransfer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawingTogether.Net
+{
+    internal class LineDataTransfer : DTO
+    {
+        public string Type => nameof(LineDataTransfer);
+
+        List<LinePortion> Portions { get; set; }
+        public LineDataTransfer(List<LinePortion> portions)
+        {
+            Portions = portions;
+        }
+    }
+}
