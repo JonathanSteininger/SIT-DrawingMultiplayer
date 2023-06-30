72b1a7164c45b71362c0bee3877dc6fc32736720
Jonathan Steininger <jonathan.steininger@icloud.com>
2023-06-30 17:31:18 +1200

    Added a stage object to handle all stored controls on a page.

--- AppDrawingTogether/Game/Stage.cs
+++ AppDrawingTogether/Game/Stage.cs
@@ -4,11 +4,97 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace AppDrawingTogether.Game
 {
     internal class Stage : GroupBox
     {
+        private new MyControlManager Controls = new MyControlManager();
+        public Stage(Size size)
+        {
+            Size = size;
+            Location = new Point(0, 0);
+        }
 
+        /// <summary>
+        /// Adds a control to the controller and controller dictionary
+        /// </summary>
+        /// <param name="control">controll being added</param>
+        public void AddControl(Control control)
+        {
+            Controls.Add(control.Name, control);
+            base.Controls.Add(control);
+        }
+        /// <summary>
+        /// Removes control from the stage controler.
+        /// removes control from the controls dictionary
+        /// </summary>
+        /// <param name="control">control being removed</param>
+        public void RemoveControl(Control control)
+        {
+            Controls.Remove(control.Name);
+            base.Controls.Remove(control);
+        }
+
+        /// <summary>
+        /// Checks if The controll passed is contained in the dictionary.
+        /// </summary>
+        /// <param name="control">control being checked for</param>
+        /// <returns>true if found. false otherwise.</returns>
+        public new bool Contains(Control control) => Contains(control.Name);
+        /// <summary>
+        /// Checks if the controls contains that key
+        /// </summary>
+        /// <param name="Key">key</param>
+        /// <returns>true if it contains the key. false otherwise</returns>
+        public bool Contains(string Key)
+        {
+            return Controls.ContainsKey(Key);
+        }
+
+        /// <summary>
+        /// returns the control based off the key which is the controls name.
+        /// </summary>
+        /// <param name="key">key</param>
+        /// <returns>contorl with that key</returns>
+        public Control this[string key] => GetControl(key);
+        /// <summary>
+        /// Returns the control based off the key which is the controls name.
+        /// </summary>
+        /// <param name="name">Key</param>
+        /// <returns>Control with that key</returns>
+        public Control GetControl(string name)
+        {
+            return Controls[name];
+        }
+
+        public delegate void ControlAction(Control control);
+        /// <summary>
+        /// Loops through every control. can add a custom method to be looped through
+        /// </summary>
+        /// <param name="action">Method used for each control</param>
+        public void ForEachControl(ControlAction action)
+        {
+            foreach(KeyValuePair<string, Control> pair in Controls)
+            {
+                action.Invoke(pair.Value);
+            }
+        }
+        /// <summary>
+        /// Used to store all controls stored in this class
+        /// </summary>
+        internal class MyControlManager : Dictionary<string, Control>
+        {
+            public MyControlManager() : base() { }
+            /// <summary>
+            /// Uses the Controls Name as a key
+            /// </summary>
+            /// <param name="control">control to be added</param>
+            public void Add(Control control)
+            {
+                base.Add(control.Name, control);
+            }
+        }
     }
 }
